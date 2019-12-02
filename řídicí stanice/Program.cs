9e896c3472c13fb8397e34efb7fae54356221486
kukol.tomas.2017 <kukol.tomas.2017@UC404PC16>
2019-12-02 16:42:12 +0100

    Update

---
+++ řídicí stanice/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace řídicí_stanice
+{
+    class TimeOfTick : EventArgs
+    {
+        public bool Rain { get; set; }
+        public bool Night { get; set; }
+    }
+    class Car
+    {
+        public float Speed { get; set; }
+        public bool Lights { get; set; }
+        public int RouteLength { get; set; }
+    }
+    class Headquater
+    {
+        public void Subscribe(WeatherStation w)
+        {
+            w.WS += new WeatherStation.WeatherStatus(HeardIt);
+        }
+
+        private void HeardIt(WeatherStation w, TimeOfTick e)
+        {
+            Console.WriteLine("{0}", e.Rain);
+        }
+    }
+    class WeatherStation
+    {
+        public delegate void WeatherStatus(WeatherStation w, TimeOfTick e);
+        public event WeatherStatus WS;
+
+    }
+    class Road
+    {
+
+    }
+}
